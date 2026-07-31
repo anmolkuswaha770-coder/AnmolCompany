d7656454ecfe3db9cb93ce1f480a3dff948677e6
anmolkuswaha770-coder <anmolkuswaha770@gmail.com>
2026-07-31 12:33:58 +0530

    Add MainActivity for Offline Mesh Network initialization

---
+++ MainActivity.kt
@@ -0,0 +1,11 @@
+package com.anmolcompany.meshapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Anmol Company - Offline Mesh Network Initialization
+    }
+}
